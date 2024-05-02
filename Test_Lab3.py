b2be036f553cf9dc5fc291a41fe53bf3f0754367
Shune Lai Hlaing Hmee <hlainghmees.23@ichat.sp.edu.sg>
2024-05-02 16:48:03 +0800

    new tests

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -28,3 +28,20 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_greater_equal_ten():
+    input_arr = [1,43,2,4,3,2,3,4,3,23,3,43,2,32]
+    
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+    assert (result == 1)
+
+
+def test_bubble_zero():
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+    assert (result == 0)
+
+def test_bubble_integer():
+    input_arr = [2,3,0.23,23.5]
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+    assert (result == 2)
